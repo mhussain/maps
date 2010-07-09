4931474a4d1244725974f3f4ab0b36b50758fe66
Mujtaba Hussain <mujtaba.hussain@rea-group.com>
2010-07-09 14:33:32 +1000

    Changed zoom level to 12

--- js/find_location.js
+++ js/find_location.js
@@ -39,7 +39,7 @@ function submitGeocode(control, map) {
               var lng = location.c;
               var center = new google.maps.LatLng(lat, lng);
               map.setCenter(center);
-              map.setZoom(9);
+              map.setZoom(12);
               found_in_oz = true;
               break;
             }
